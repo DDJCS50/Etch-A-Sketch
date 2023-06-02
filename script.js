275ac1b0d1ca27c99daa7c5e083b5ff8f8c1e5ce
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-02 08:17:45 -0700

    Add row creation func
    Added row class with flex properties in order to apply it to the new
    rows that are going to be created. Adjusted the sandbox flex properties
    in order to make the grid centered. Added function for creating rows,
    applying the row class to each in every iteration, calling the
    createGridSquare function every iteration in a nested loop, and finally
    appending the row to the sandbox every iteration

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function createGridSquare() {
-    sandbox = document.querySelector('div.sandbox');
     gridSquare = document.createElement('div');
     gridSquare.setAttribute('class', 'square');
-    sandbox.appendChild(gridSquare);
+    gridRow.appendChild(gridSquare);
 }
 
-for (let i = 0; i < 16; i++) {
-    createGridSquare();
+let gridRow;
+
+function createGridRow() {
+    for (let j = 0; j < 16; j++) {
+        gridRow = document.createElement('div');
+        gridRow.setAttribute('class', 'row');
+        for (let i = 0; i < 16; i++) {
+            createGridSquare();
+        }
+        sandbox = document.querySelector('div.sandbox');
+        sandbox.appendChild(gridRow);
+    }
 }
+
+createGridRow();
